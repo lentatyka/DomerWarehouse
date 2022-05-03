9d8267aa0f1901e2ce2f9ada8222408581fb0f56
lentatyka <50218140+lentatyka@users.noreply.github.com>
2022-05-03 19:57:54 +0500

    Update SampleWorkerFactory.kt

--- app/src/main/java/com/lentatyka/domerwarehouse/data/main/background/worker/SampleWorkerFactory.kt
+++ app/src/main/java/com/lentatyka/domerwarehouse/data/main/background/worker/SampleWorkerFactory.kt
@@ -7,18 +7,6 @@ import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
 import javax.inject.Inject
 
-/**
- * If there is no worker found, return null to use the default behaviour of [WorkManager]
- * (create worker using refection)
- *
- * In addition you can use dagger multi-binding to avoid manual check the workerClassName
- * but if you not familiar dagger multi-binding then it better to do it manually since
- * it easier to understand.
- *
- * Check out earlier commit to see the dagger multi-binding solution!
- *
- * @see WorkerFactory.createWorkerWithDefaultFallback
- */
 class SampleWorkerFactory @Inject constructor(
     private val helloWorldWorkerFactory: FirebaseWorker.Factory,
 ) : WorkerFactory() {
